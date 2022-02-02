1088f418c27a0bfd15e9293b799aa0d4c1512bcd
Diego Vieira <dgavieira@users.noreply.github.com>
2022-02-02 19:58:27 -0400

    Criação de aluno no formulário Salvando Alunos com Modelo DAO Classe Aluno Modificações de layout em activity_formulario_aluno.xml para aparecer na tela

--- app/src/main/java/com/dgavieira/agenda/ui/activity/FormularioAlunoActivity.java
+++ app/src/main/java/com/dgavieira/agenda/ui/activity/FormularioAlunoActivity.java
@@ -2,8 +2,14 @@ package com.dgavieira.agenda.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 import com.dgavieira.agenda.R;
+import com.dgavieira.agenda.dao.AlunoDAO;
+import com.dgavieira.agenda.model.Aluno;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class FormularioAlunoActivity extends AppCompatActivity {
 
@@ -11,5 +17,28 @@ public class FormularioAlunoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_aluno);
+
+        AlunoDAO dao = new AlunoDAO();
+
+        final EditText campoNome = findViewById(R.id.activity_formulario_aluno_nome);
+        final EditText campoTelefone = findViewById(R.id.activity_formulario_aluno_telefone);
+        final EditText campoEmail = findViewById(R.id.activity_formulario_aluno_email);
+
+        Button botaoSalvar = findViewById(R.id.activity_formulario_aluno_botao_salvar);
+        botaoSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nome = campoNome.getText().toString();
+                String telefone = campoTelefone.getText().toString();
+                String email = campoEmail.getText().toString();
+
+                Aluno alunoCriado = new Aluno(nome, telefone, email);
+                dao.salva(alunoCriado);
+
+                startActivity(new Intent(FormularioAlunoActivity.this,
+                        ListaAlunosActivity.class));
+
+            }
+        });
     }
 }
--- app/src/main/java/com/dgavieira/agenda/ui/activity/ListaAlunosActivity.java
+++ app/src/main/java/com/dgavieira/agenda/ui/activity/ListaAlunosActivity.java
@@ -8,24 +8,22 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.dgavieira.agenda.R;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import com.dgavieira.agenda.dao.AlunoDAO;
 
 public class ListaAlunosActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_alunos);
-        setTitle("Lista de Alunos");
-        List<String> alunos = new ArrayList<>(Arrays.asList("Alex", "Fran", "José", "Maria", "Ana"));
 
-        ListView listadeAlunos = findViewById(R.id.activity_main_listview);
+        AlunoDAO dao = new AlunoDAO();
+        setTitle("Lista de Alunos");
+
+        ListView listadeAlunos = findViewById(R.id.activity_lista_alunos_listview);
         listadeAlunos.setAdapter(new ArrayAdapter<>(
                 this,
                 android.R.layout.simple_list_item_1,
-                alunos));
+                dao.todos()));
 
     }
 }
